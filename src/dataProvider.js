f5f7adb08520df30221bc2a723f4fcaffe845d7e
Somkid Simajarn <sisomkid@bigc.co.th>
2022-04-27 17:40:35 +0700

    update dataProvider

--- src/dataProvider.js
+++ src/dataProvider.js
@@ -1,7 +1,7 @@
 import { fetchUtils } from 'react-admin';
 import { stringify } from 'query-string';
 
-const apiUrl = 'https://my.api.com/';
+const apiUrl = 'https://jsonplaceholder.typicode.com';
 const httpClient = fetchUtils.fetchJson;
 
 export default {
@@ -15,22 +15,30 @@ export default {
         };
         const url = `${apiUrl}/${resource}?${stringify(query)}`;
 
-        return httpClient(url).then(({ headers, json }) => ({
-            data: json,
-            total: parseInt(headers.get('content-range').split('/').pop(), 10),
-        }));
+        console.log("getList :", url, params)
+        return httpClient(url).then(({ headers, json }) => 
+            {
+                return {
+                    data: json,
+                    total: json.length//parseInt(headers.get('content-range').split('/').pop(), 10),
+                }
+            }
+        );
     },
 
-    getOne: (resource, params) =>
-        httpClient(`${apiUrl}/${resource}/${params.id}`).then(({ json }) => ({
+    getOne: (resource, params) =>{
+        console.log("getOne :", resource, params)
+        return httpClient(`${apiUrl}/${resource}/${params.id}`).then(({ json }) => ({
             data: json,
-        })),
+        }));
+    },
 
     getMany: (resource, params) => {
         const query = {
             filter: JSON.stringify({ id: params.ids }),
         };
         const url = `${apiUrl}/${resource}?${stringify(query)}`;
+        console.log("getMany :", resource, params, url)
         return httpClient(url).then(({ json }) => ({ data: json }));
     },
 
@@ -47,42 +55,50 @@ export default {
         };
         const url = `${apiUrl}/${resource}?${stringify(query)}`;
 
+        console.log("getManyReference :", resource, params, url)
         return httpClient(url).then(({ headers, json }) => ({
             data: json,
             total: parseInt(headers.get('content-range').split('/').pop(), 10),
         }));
     },
 
-    update: (resource, params) =>
-        httpClient(`${apiUrl}/${resource}/${params.id}`, {
+    update: (resource, params) =>{
+        console.log("update :", resource, params)
+        return    httpClient(`${apiUrl}/${resource}/${params.id}`, {
             method: 'PUT',
             body: JSON.stringify(params.data),
-        }).then(({ json }) => ({ data: json })),
+        }).then(({ json }) => ({ data: json }))
+    },
 
     updateMany: (resource, params) => {
         const query = {
             filter: JSON.stringify({ id: params.ids}),
         };
+
+        console.log("updateMany :", resource, params)
         return httpClient(`${apiUrl}/${resource}?${stringify(query)}`, {
             method: 'PUT',
             body: JSON.stringify(params.data),
         }).then(({ json }) => ({ data: json }));
     },
 
-    create: (resource, params) =>
-        httpClient(`${apiUrl}/${resource}`, {
+    create: (resource, params) =>{
+        console.log("create :", resource, params)
+        return httpClient(`${apiUrl}/${resource}`, {
             method: 'POST',
             body: JSON.stringify(params.data),
         }).then(({ json }) => ({
             data: { ...params.data, id: json.id },
-        })),
-
-    delete: (resource, params) =>
-        httpClient(`${apiUrl}/${resource}/${params.id}`, {
+        }))
+    },
+    delete: (resource, params) =>{
+        console.log("delete :", resource, params)
+        return httpClient(`${apiUrl}/${resource}/${params.id}`, {
             method: 'DELETE',
-        }).then(({ json }) => ({ data: json })),
-
+        }).then(({ json }) => ({ data: json }))
+    },
     deleteMany: (resource, params) => {
+        console.log("deleteMany :", resource, params)
         const query = {
             filter: JSON.stringify({ id: params.ids}),
         };
