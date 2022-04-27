942c0366777c42a1ddcd9f394c2104f9ce45e01d
Somkid Simajarn <sisomkid@bigc.co.th>
2022-04-27 16:24:35 +0700

    update submodule

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+import * as React from "react"
+import {Admin, Resource, ListGuesser, EditGuesser } from "react-admin"
+import jsonServerProvider from "ra-data-json-server"
+
+import PostIcon from '@mui/icons-material/Group';
+
+import authProvider from './authProvider';
+import { Dashboard } from './Dashboard';
+import { UserList } from "./users"
+import { PostList, PostCreate, PostEdit } from './posts';
+
+// const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com")
+
+import dataProvider from './dataProvider';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
+       <Resource name="users" list={UserList} edit={EditGuesser} ></Resource>
+       <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}></Resource>
+     </Admin>
     </div>
   );
 }
